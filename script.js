02f90fcc7c704ee52d396bdf688f3352e3a7a8a9
vricheb973 <vricheb973@g.educaand.es>
2026-04-29 12:30:41 +0200

    formulario añadir alumno

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 let alumnos = Array.from(document.getElementsByClassName("alumno"));
 let elementoArrastrando = null;
-
-//Empiezo a arrastrar
+ 
+//Empiezo a arrastrar (1er evento)
 alumnos.forEach(alumno => {
     alumno.addEventListener("dragstart", (e)=> {
         elementoArrastrando = e.target;
     });
 });
 
-//Estoy arrastrando
+//Termino de arrastrar (4o evento)
+alumnos.forEach(alumno => {
+    alumno.addEventListener("dragend", (e)=> {
+        elementoArrastrando = null;
+    });
+});
+
+//Al pasar por encima (2o evento)
 let columnas = Array.from(document.getElementsByClassName("column"));
 
 columnas.forEach(columna => {
@@ -17,7 +24,7 @@ columnas.forEach(columna => {
     });
 })
 
-//Suelto 
+//Al soltar sobre un elemento  (3er evento)
 columnas.forEach(columna => {
     columna.addEventListener("drop", (e)=>{
         let lista = columna.querySelector("ul");
@@ -25,9 +32,47 @@ columnas.forEach(columna => {
     });
 });
 
-//Al finalizar
-alumnos.forEach(alumno => {
-    alumno.addEventListener("dragend", (e)=> {
+//Al abrir la página por primera vez
+window.onload = ()=> {
+    document.getElementById("form").style.display = "none";
+}
+
+//Al pulsar nuevo (abre el formulario)
+document.getElementById("new").addEventListener("click", ()=>{
+    document.getElementById("form").style.display = "block";
+});
+
+//Al pulsar añadir (añade a la lista de alumnos y cierra el formulario)
+document.getElementById("add").addEventListener("click", (e)=>{
+    e.preventDefault();
+    let li = document.createElement("li");
+    li.textContent = document.getElementById("nombre").value + " " 
+        + document.getElementById("apellidos").value + " "
+        + document.getElementById("edad").value + " ";
+    
+    li.draggable = true;
+    li.classList.add("alumno");
+    li.style.backgroundColor = this.generarColorAleatorio();
+
+    li.addEventListener("dragstart", (e)=> {
+        elementoArrastrando = e.target;
+    });
+
+    li.addEventListener("dragend", (e)=> {
         elementoArrastrando = null;
     });
-});
+
+    document.getElementById("listaAll").appendChild(li);
+
+
+    document.getElementById("form").reset();
+    document.getElementById("form").style.display = "none";
+})
+
+function generarColorAleatorio(){
+    let colores = ["lightblue","lightcoral", "lightcyan", "lightgoldenrodyellow", "lightgray", "lightgreen", "lightpink", "lightsalmon", "lightseagreen", "lightskyblue"];
+    let num = Math.floor(Math.random() * colores.length);
+
+    return colores[num];
+
+}
